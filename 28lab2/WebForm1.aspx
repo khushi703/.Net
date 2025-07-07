850c05c109b2d6f31591f0c5741fd9bcbfb95051
Student <Student@DESKTOP-4DFCQ3G>
2025-07-07 15:04:52 +0530

    Add project files.

---
+++ 28lab2/WebForm1.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="_28lab2.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <script>
+                function checkTextBox2Value() {
+                    var tb2 = document.getElementById('<%= TextBox2.ClientID %>');
+                    var divBtn = document.getElementById('<%= Button5.ClientID %>');
+
+                    if (tb2.value === "0") {
+                      tb2.style.backgroundColor = "red";
+                      divBtn.disabled = true;
+                    } else {
+                      tb2.style.backgroundColor = "";
+                      divBtn.disabled = false;
+                    }
+                  }
+            </script>
+            form
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:TextBox ID="TextBox2" runat="server" onkeyup="checkTextBox2Value()"></asp:TextBox >
+            <asp:TextBox ID="TextBox3" runat="server"></asp:TextBox>
+            <asp:Button ID="Button2" runat="server" Text="+" onClick="addition"/>
+            <asp:Button ID="Button3" runat="server" Text="-" onClick="subtraction" />
+            <asp:Button ID="Button4" runat="server" Text="*" onClick="multiplication"/>
+            <asp:Button ID="Button5" runat="server" Text="/" onClick="division"/>
+   
+        </div>
+    </form>
+</body>
+</html>
---
+++ 28lab2/WebForm1.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _28lab2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void CheckTextBox2Value()
+        {
+            if (int.TryParse(TextBox2.Text, out int value2) && value2 == 0)
+            {
+                TextBox2.BackColor = System.Drawing.Color.Red;  
+                Button5.Enabled = false;                        
+            }
+
+        }
+        protected void addition(object sender, EventArgs e)
+        {
+            string input1 = TextBox1.Text;
+            string input2 = TextBox2.Text;
+            TextBox3.Text = "";
+            if (int.TryParse(input1, out int value1) && int.TryParse(input2, out int value2))
+            {
+                int sum = value1 + value2;
+                TextBox3.Text = sum.ToString(); 
+            }
+            else
+            {
+                TextBox3.Text = "Invalid input";
+            }
+        }
+        protected void subtraction(object sender, EventArgs e)
+        {
+            string input1 = TextBox1.Text;
+            string input2 = TextBox2.Text;
+            TextBox3.Text = "";
+            if (int.TryParse(input1, out int value1) && int.TryParse(input2, out int value2))
+            {
+                int sum = value1 - value2;
+                TextBox3.Text = sum.ToString(); 
+            }
+            else
+            {
+                TextBox3.Text = "Invalid input";
+            }
+        }
+        protected void multiplication(object sender, EventArgs e)
+        {
+            string input1 = TextBox1.Text;
+            string input2 = TextBox2.Text;
+            TextBox3.Text = "";
+            if (int.TryParse(input1, out int value1) && int.TryParse(input2, out int value2))
+            {
+                int sum = value1 * value2;
+                TextBox3.Text = sum.ToString();
+            }
+            else
+            {
+                TextBox3.Text = "Invalid input";
+            }
+        }
+        protected void division(object sender, EventArgs e)
+        {
+            
+            string input1 = TextBox1.Text;
+            string input2 = TextBox2.Text;
+            TextBox3.Text = "";
+            if (int.TryParse(input1, out int value1) && int.TryParse(input2, out int value2))
+            {
+
+                int sum = value1 / value2;
+                TextBox3.Text = sum.ToString(); 
+            }
+            else
+            {
+                TextBox3.Text = "Invalid input";
+            }
+        }
+    }
+}
