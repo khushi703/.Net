5027779a7e9efaa65ac74328058abbfc894420fd
diyaprajapati <diyabprajapati2005@gmail.com>
2025-09-08 15:17:13 +0530

    q2 c

---
+++ Sessional2 Q3/Sessional2 Q3/q2_c.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="q2_c.aspx.cs" Inherits="Sessional2_Q3.q2_c" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Product Management</title>
+    <style>
+        table, th, td { border: 1px solid black; border-collapse: collapse; padding:5px; }
+        th { background-color: #f2f2f2; }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Product Management</h2>
+        <asp:Label ID="lblMsg" runat="server" ForeColor="Red" /><br /><br />
+
+        <table>
+            <tr>
+                <td>Product Name:</td>
+                <td><asp:TextBox ID="txtName" runat="server" /></td>
+            </tr>
+            <tr>
+                <td>Product Cost:</td>
+                <td><asp:TextBox ID="txtCost" runat="server" /></td>
+            </tr>
+            <tr>
+                <td>Product ID (for Update/Delete):</td>
+                <td><asp:TextBox ID="txtId" runat="server" /></td>
+            </tr>
+        </table>
+        <br />
+        <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+        <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+
+        <h3>All Products</h3>
+        <asp:GridView ID="GridViewProducts" runat="server" AutoGenerateColumns="False">
+            <Columns>
+                <asp:BoundField DataField="P_Id" HeaderText="Product ID" ReadOnly="True" />
+                <asp:BoundField DataField="P_Name" HeaderText="Product Name" />
+                <asp:BoundField DataField="P_Cost" HeaderText="Product Cost" />
+            </Columns>
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Sessional2 Q3/Sessional2 Q3/q2_c.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+
+namespace Sessional2_Q3
+{
+    public partial class q2_c : System.Web.UI.Page
+    {
+        string conn = ConfigurationManager.ConnectionStrings["ProductDbConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid();
+            }
+        }
+
+        private void BindGrid()
+        {
+            using (ProductDataDataContext db = new ProductDataDataContext(conn))
+            {
+                GridViewProducts.DataSource = db.Products.ToList();
+                GridViewProducts.DataBind();
+            }
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ProductDataDataContext db = new ProductDataDataContext(conn))
+                {
+                    Product newProduct = new Product
+                    {
+                        P_Name = txtName.Text.Trim(),
+                        P_Cost = Convert.ToDecimal(txtCost.Text.Trim())
+                    };
+                    db.Products.InsertOnSubmit(newProduct);
+                    db.SubmitChanges();
+                }
+
+                lblMsg.Text = "Product inserted successfully.";
+                ClearFields();
+                BindGrid();
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Error: " + ex.Message;
+            }
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ProductDataDataContext db = new ProductDataDataContext(conn))
+                {
+                    int id = Convert.ToInt32(txtId.Text.Trim());
+                    Product prod = db.Products.SingleOrDefault(p => p.P_Id == id);
+
+                    if (prod != null)
+                    {
+                        prod.P_Name = txtName.Text.Trim();
+                        prod.P_Cost = Convert.ToDecimal(txtCost.Text.Trim());
+                        db.SubmitChanges();
+
+                        lblMsg.Text = "Product updated successfully.";
+                        ClearFields();
+                        BindGrid();
+                    }
+                    else
+                    {
+                        lblMsg.Text = "Product ID not found.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Error: " + ex.Message;
+            }
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (ProductDataDataContext db = new ProductDataDataContext(conn))
+                {
+                    int id = Convert.ToInt32(txtId.Text.Trim());
+                    Product prod = db.Products.SingleOrDefault(p => p.P_Id == id);
+
+                    if (prod != null)
+                    {
+                        db.Products.DeleteOnSubmit(prod);
+                        db.SubmitChanges();
+
+                        lblMsg.Text = "Product deleted successfully.";
+                        ClearFields();
+                        BindGrid();
+                    }
+                    else
+                    {
+                        lblMsg.Text = "Product ID not found.";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                lblMsg.Text = "Error: " + ex.Message;
+            }
+        }
+
+        private void ClearFields()
+        {
+            txtId.Text = "";
+            txtName.Text = "";
+            txtCost.Text = "";
+        }
+    }
+}
