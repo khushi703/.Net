8185dc7e38ce0139dbf5362ffeb971ac69080549
diyaprajapati <diyabprajapati2005@gmail.com>
2025-09-08 15:00:38 +0530

    q3 or a

---
+++ Sessional2 Q3/Sessional2 Q3/q3_ora.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="q3_ora.aspx.cs" Inherits="Sessional2_Q3.q3_ora" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <h2>Update Employee Details</h2>
+        <asp:Label ID="lblMsg" runat="server" ForeColor="Red" /><br /><br />
+
+        <asp:Label ID="lblEmpId" runat="server" Text="Enter Emp Id: " />
+        <asp:TextBox ID="txtEmpId" runat="server" /><br /><br />
+
+        <asp:Label ID="lblEmpName" runat="server" Text="New Employee Name: " />
+        <asp:TextBox ID="txtEmpName" runat="server" /><br /><br />
+
+        <asp:Label ID="lblEmpDesg" runat="server" Text="New Designation: " />
+        <asp:TextBox ID="txtEmpDesg" runat="server" /><br /><br />
+
+        <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+    </form>
+</body>
+</html>
---
+++ Sessional2 Q3/Sessional2 Q3/q3_ora.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Sessional2_Q3
+{
+    public partial class q3_ora : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            string empId = txtEmpId.Text.Trim();
+            string newName = txtEmpName.Text.Trim();
+            string newDesg = txtEmpDesg.Text.Trim();
+
+            if (string.IsNullOrEmpty(empId) || string.IsNullOrEmpty(newName) || string.IsNullOrEmpty(newDesg))
+            {
+                lblMsg.Text = "All fields are required.";
+                return;
+            }
+
+            string connString = ConfigurationManager.ConnectionStrings["dbCon"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connString))
+            {
+                string query = "UPDATE Employee SET Emp_Name=@EmpName, Emp_Desg=@EmpDesg WHERE Emp_Id=@EmpId";
+
+                using (SqlCommand cmd = new SqlCommand(query, con))
+                {
+                    // Parameterized query prevents SQL injection
+                    cmd.Parameters.AddWithValue("@EmpName", newName);
+                    cmd.Parameters.AddWithValue("@EmpDesg", newDesg);
+                    cmd.Parameters.AddWithValue("@EmpId", empId);
+
+                    con.Open();
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if (rowsAffected > 0)
+                        lblMsg.Text = "Employee updated successfully!";
+                    else
+                        lblMsg.Text = "Emp_Id not found.";
+                }
+            }
+        }
+    }
+}
