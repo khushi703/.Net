2da32b2af34278dcf575afe331bfd5641722578e
diyaprajapati <diyabprajapati2005@gmail.com>
2025-09-21 17:03:02 +0530

    fix prob

--- lab10/lab10/App_Start/RouteConfig.cs
+++ lab10/lab10/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace lab10
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Calculator", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Cache", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
